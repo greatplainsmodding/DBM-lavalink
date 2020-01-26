37ecd74437dfd137e488b208bcb810327caf7b12
greatplainsmodding <greatplainsmodding@gmail.com>
2020-01-25 20:06:21 -0600

    update

--- lavalinkExtensions/lavalink_extension.js
+++ lavalinkExtensions/lavalink_extension.js
@@ -122,9 +122,10 @@ module.exports = {
             port: 3000,
             password: 'TheMonDonSmart'
         }]
+        
         DBM.Bot.bot.lavalink = new PlayerManager(DBM.Bot.bot, nodes, {
             user: DBM.Bot.bot.user.id,
             shards: 0
         })
     }
-}; // u stupid :reverse: :AngryBoi:
+}
